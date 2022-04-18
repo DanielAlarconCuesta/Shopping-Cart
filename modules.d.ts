66c512f0d34e021500d7be80f6b68b087ceaea5b
Daniel1945 <danielalarconcuesta@gmail.com>
2022-04-18 17:18:04 +0200

    Environment variables and typescript config

---
+++ modules.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        REACT_APP_COMPANY_SERVICE_HOST?: string;
+        REACT_APP_ITEM_SERVICE_HOST?: string;
+        REACT_APP_TAG_SERVICE_HOST?: string;
+    }
+}
